932a77401b26fa1c0bad4fc1d0680b97c32eee1b
M1123 <dbv93@yandex.ru>
2019-05-25 07:12:20 +0700

    CRD

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 $(()=>{
     $("#add").on("click", ()=>{
-        let key = new Date();
+        let key= new Date();
         let val=prompt('Заметка');
         localStorage.setItem(key,val);
         location.reload();
     })
     for (let i=0;i<localStorage.length;i++){
-        key = localStorage.key(i);
-        let elem=$('<div class="card"></div>').text(localStorage.getItem(key));
-        $(elem).append('<hr><header>'+key+'</header>');
+        let key = localStorage.key(i);
+        let elem=$('<div class="card"></div>');
+        $(elem).append('<div class="card-body">'+localStorage.getItem(key)+'</div>');
+        $(elem).append('<div class="card-footer bg-transparent"><small class="text-muted">'+key+'</small></div>');
         $(elem).append('<button class="del">x</button>');
         $("#myList").append(elem);
     }
-    $(".del").on("click",()=>{
-        $(this).parent().remove();
-        console.log('remove');
+    $(".del").on("click",function(){      
+        let key = $(this).siblings().children().text();
+        localStorage.removeItem(key);
+        location.reload();
     })
+    
+    // $(".card").on(
+    //     "blur", function(){
+    //         let key = $(this).child().text();
+    //         let val=prompt('Заметка');
+    //         localStorage.setItem(key,val);
+    //         // location.reload();
+    //     }
+    // )
 })
